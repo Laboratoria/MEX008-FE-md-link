ac3f8f5642cd91a0c596e3ac88739ac7b974f7e5
trinitysilvia <trinitysilviag@gmail.com>
2019-09-03 14:07:41 -0500

    implementando funcion fs

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 */
 // __tests__/index.test.js
 
+
+//Funcion para leer archivo md y visualizarlo en consola
+let fs = require('fs');
+
+fs.readFile('readme-links.md', 'utf8', (err, dta) => {
+  if(err) {
+    console.log('error: ' err);
+  }
+  else {
+    console.log(data);
+  }
+  });
