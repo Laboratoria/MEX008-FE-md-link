b7523a03af452e980b37e7f7a4d7e93a963bf7b4
trinitysilvia <trinitysilviag@gmail.com>
2019-09-03 16:08:27 -0500

    creando carpeta lib y primer archivo readfile.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-/*module.exports = () => {
+module.exports = () => {
   // ...
 };
-*/
-// __tests__/index.test.js
 
-
-//Funcion para leer archivo md y visualizarlo en consola
-const  fs = require('fs');
-
-fs.readFile('readme-links.md', 'utf-8', (error, data) => {
-  if(error) {
-    console.log('Error ${error}');
-  }else{
-    console.log(data);
-  }
-});
