3f54ed96c4c17a7d0f74d4ce06993bff71262c87
trinitysilvia <trinitysilviag@gmail.com>
2019-09-09 09:17:49 -0500

    agregando funcion mdlinks en archivo de index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-//FUNCTION
+import { searchDirectoryPath } from "./searchlinks.js";
+import { validateLinks } from "./optionsfile,js";
+
+
+export const mdLinks = (path, options) => {
+    if(options.validate) {
+        return validateLinks(path);
+    } else {
+        return new Promise(resolve => resolve(searchDirectoryPath(path)));
+    }
+};
