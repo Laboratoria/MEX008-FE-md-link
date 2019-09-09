fc3002bdee8fc2161e67171f30b6d275e0b5b9fa
trinitysilvia <trinitysilviag@gmail.com>
2019-09-09 08:40:24 -0500

    PATHFILE.JS - se han separado las funciones en pequeños bloques de acuerdo a su funcionamiento

--- src/pathfile.js
+++ src/pathfile.js
@@ -2,7 +2,7 @@ const fs = require ('fs');
 const path = require('path');
 
 //FUNCION SI LA RUTA es ABSOLUTA
-export const checkPathisAbsolute = userPath => {
+export const checkPathIsAbsolute = userPath => {
     //"path.isAbsolute"--> determina si la ruta es absoluta
     const userPathAbsolute = path.isAbsolute(userPath);
     return userPathAbsolute;
@@ -11,9 +11,51 @@ export const checkPathisAbsolute = userPath => {
 //CONVERTIR LA FUNCION RELATIVA --> ABSOLUTA
 export const checkPathRelative = userPath => {
     // "path.resolve"--> vuelve la ruta relativa a absoluta
-    const newPathIsAbsolute = path.resolve(userPath);
-    return newPathIsAbsolute;
+    const newPathAbsolute = path.resolve(userPath);
+    return newPathAbsolute;
 };
+
+//FUNCION QUE VA LEER SI LA RUTA ES UN ARCHIVO
+export const userPathFile = userPath => {
+    //"fs.IstatSync"-->
+    const filePathStatus = fs.IstatSync(userPath);
+    // ".isFile"-->
+    const checkFilePath = filePathStatus.isFile();
+    
+    return checkFilePath;
+};
+
+//FUNCION QUE VA A REVISAR SI ES UN DIRECTORIO
+export const userPathDirectory = userPath => {
+    //
+    const directoryPathStatus = fs.IstatSync(userPath);
+    //
+    const checkDirectoryPath = directoryPathStatus.isDirectory();
+
+    return checkDirectoryPath;
+};
+//FUNCION PARA LEER EL DIRECTORIO
+export const searchDirectoryPath = (dirRoute) => {
+    let arrayFile = [];
+
+    if (!userPathDirectory(dirRoute)) {
+        arrayFile.push(dirRoute);
+    } else {
+        const files = fs.readdirSync(dirRoute);
+
+        for (let i= 0; i < files.length; i++) {
+            const newPath = path.join(dirRoute, file[i]);
+
+            if (userPathDirectory(newPath)) {
+                arrayFile= arrayFile.concat(searchDirectoryPath(newPath));
+            } else {
+                arrayFile.push(newPath);
+            };
+        };
+    };
+    return arrayFile;
+};
+
 
 
 
