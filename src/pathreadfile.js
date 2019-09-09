cc7184e20c627d010376ba0ce47b5d19a2c1b75f
trinitysilvia <trinitysilviag@gmail.com>
2019-09-08 21:51:38 -0500

    cambiando nombre a archivo

---
+++ src/pathreadfile.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+//const chalk = require('chalk');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*---------------------------------------------------
+-----------------------------------------------------
+const readDocumentsLinks= () => {
+  fs.readdir('./', (error, files) => {
+    if(error) {
+      throw error;
+    }
+    console.log(files);
+
+    fs.readFile('../readme-links.md', 'UTF-8', (error,file) => {
+      if(error) {
+        throw error;
+      }
+      console.log(chalk.black.bgGreenBright(file));
+    });
+    console.log(chalk.cyan('Contenido del archivo ....'));
+  });
+};
+readDocument();
+*/
